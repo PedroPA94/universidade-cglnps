3eaf8bd4b53350871c8ee4299640b4b7a8d7b335
Pedro Papini <ppapinia@gmail.com>
2025-08-31 21:19:43 -0300

    feat: implementa pagina nao encontrada

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+    component: Home,
   },
   {
     path: '',
     pathMatch: 'full',
     redirectTo: 'home',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/pagina-nao-encontrada/pagina-nao-encontrada').then(
+        (m) => m.PaginaNaoEncontrada
+      ),
   },
 ];
