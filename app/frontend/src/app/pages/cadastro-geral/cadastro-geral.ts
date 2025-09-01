1accaee403eef63a3a56a4566bdca045c0b9f529
Pedro Papini <ppapinia@gmail.com>
2025-08-31 22:10:41 -0300

    chore: adiciona inputs na pagina

--- app/frontend/src/app/pages/cadastro-geral/cadastro-geral.ts
+++ app/frontend/src/app/pages/cadastro-geral/cadastro-geral.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Layout } from '../../components/layout/layout';
 import { Card } from '../../components/card/card';
 import { Botao } from '../../components/botao/botao';
+import { RouterLink } from '@angular/router';
+import { Input } from '../../components/input/input';
 
 @Component({
   selector: 'app-cadastro-geral',
-  imports: [Layout, Card, Botao],
+  imports: [Layout, Card, Botao, RouterLink, Input],
   templateUrl: './cadastro-geral.html',
   styleUrl: './cadastro-geral.css',
 })
